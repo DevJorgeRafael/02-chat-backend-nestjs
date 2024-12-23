adf8e2a756fe662bc7be7ae60a99ec3e0cec6dc7
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-12-22 19:14:53 -0500

    estructura del proyecto con los modulos chat, database, messages, sockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MessagesModule } from './messages/messages.module';
+import { SocketsModule } from './sockets/sockets.module';
+import { ChatGateway } from './chat/chat.gateway';
+import { DatabaseModule } from './database/database.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    AuthModule,
+    UsersModule,
+    MessagesModule,
+    SocketsModule,
+    DatabaseModule,
+    CommonModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
