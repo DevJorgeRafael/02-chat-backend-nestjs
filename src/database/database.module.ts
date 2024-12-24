035726d8b4e052dab63ac690eaffd8a2ff4e849e
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-12-23 23:56:53 -0500

    using env for mongo connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
-  providers: [DatabaseService]
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      })
+    })
+  ],
+  exports: [MongooseModule],
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnModuleInit {
+  async onModuleInit() {
+    mongoose.connection.on('connected', () => {
+      console.log('>>> DB IS CONNECTED')
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('Error in DB connection: ', err.message)
+    });
+  }
+}
