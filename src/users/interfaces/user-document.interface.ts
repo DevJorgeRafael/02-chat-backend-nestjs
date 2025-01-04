6cd569dd3046efe4975bb2bbcb9558cda1fea1a1
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2025-01-03 21:54:49 -0500

    UserDocument interface done

---
+++ src/users/interfaces/user-document.interface.ts
@@ -0,0 +1,9 @@
+import { Document } from "mongoose";
+
+export interface UserDocument extends Document {
+    id: string;
+    name: string;
+    email: string;
+    password?: string;
+    online: boolean;
+}
