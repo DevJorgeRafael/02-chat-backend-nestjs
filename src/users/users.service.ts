f93f17513fde2fe22be3cac6700508c6f5f815fe
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2025-01-04 21:49:01 -0500

    fixed bug using interface userDocument

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,42 +3,43 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserDocument } from './interfaces/user-document.interface';
 
 @Injectable()
 export class UsersService {
     constructor(
-        @InjectModel(User.name) private userModel: Model<User>
+        @InjectModel(User.name) private userModel: Model<UserDocument>
     ) { }
 
-    async createUser(createUserDTO: CreateUserDto): Promise<User | null> {
+    async createUser(createUserDTO: CreateUserDto): Promise<UserDocument | null> {
         const user = new this.userModel(createUserDTO);
         return user.save();
     }
 
-    async findById(id: string): Promise<User | null> {
+    async findById(id: string): Promise<UserDocument | null> {
         const user = await this.userModel.findById(id);
-        return user ? user.toJSON() : null;
+        return user ? (user.toObject() as UserDocument) : null;
     }
 
-    async findByEmail(email: string): Promise<User | null> {
+    async findByEmail(email: string): Promise<UserDocument | null> {
         const user = await this.userModel.findOne({ email });
-        return user || null; 
+        return user || null;
     }
 
-    async updateOnlineStatus(userId: string, isOnline: boolean): Promise<User | null> {
+    async updateOnlineStatus(userId: string, isOnline: boolean): Promise<UserDocument | null> {
         const user = await this.userModel.findByIdAndUpdate(
             userId, 
             { online: isOnline },
             { new: true } // Retornar el documento actualizado
         );
-        return user ? user.toJSON() : null;
+        return user ? (user.toObject() as UserDocument) : null;
     }
 
-    async findAll(userId: string): Promise<User[]> {
+    async findAll(userId: string): Promise<UserDocument[]> {
         const users = await this.userModel.find(
             { _id: { $ne: userId } }, // Excluir al usuario actual
             { password: 0 }
         );
-        return users.map(user => user.toJSON());
+        return users.map(user => user.toJSON() as UserDocument);
     }
 }
